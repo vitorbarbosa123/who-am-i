063f7366a729d3a58269ba100d52000e3751c5c2
Victor Hugo <victor.rangel@ccc.ufcg.edu.br>
2022-11-18 14:06:21 -0300

    Update interface.hs, partially working

--- src/View/interface.hs
+++ src/View/interface.hs
@@ -4,28 +4,29 @@
 -- O mesmo precisa selecionar opções entre 1 a 4, sendo a opção 1 de iniciar um novo jogo.
 menuPrincipal :: IO()
 menuPrincipal = do
-    putStr ("\n##############################\n")
+    putStr ("\n==============================================\n")
     putStr ("Bem vindo ao 'Who am i?'!\n\n")
     putStr ("[1] Novo jogo\n")
     putStr ("[2] Como Jogar\n")
     putStr ("[3] Ranking\n")
     putStr ("[4] Sair\n\n")
 
+    putStr("Digite uma opcao: ")
     opcaoEscolhidaMP <- getLine
 
-    if opcaoEscolhidaMP == 1 then do 
+    if opcaoEscolhidaMP == "1" then do 
         menuNovoJogo
         else if 
-            opcaoEscolhidaMP == 2 then 
+            opcaoEscolhidaMP == "2" then 
                 menuComoJogar
         else if 
-            opcaoEscolhidaMP == 3 then 
+            opcaoEscolhidaMP == "3" then 
                 menuRanking
         else if 
-            opcaoEscolhidaMP == 4 then 
+            opcaoEscolhidaMP == "4" then 
                 sairPrograma
         else do 
-            putStr ("Opção inválida")
+            putStr ("Opção invalida\n")
             menuPrincipal --para que o programa não termine, e retorne ao mesmo menu.
 
 
@@ -34,21 +35,23 @@ menuPrincipal = do
 -- Função mais importante, irá popular o savefile de usuários já cadastrados e
 -- irá iniciar novo jogo, nas regras de negócio
 menuNovoJogo :: IO()
-menuNovoJogo : do
+menuNovoJogo = do
+    putStr ("\n==============================================\n")
     putStr ("[1] Comecar novo jogo\n")
     putStr ("[2] Voltar ao menu principal\n\n")
 
+    putStr("Digite uma opcao: ")
     opcaoEscolhidaNJ <- getLine
 
-    if opcaoEscolhidaNJ == 1 then do
-        putStr("Insira seu nome de usuário\n")
+    if opcaoEscolhidaNJ == "1" then do
+        putStr("Insira seu nome de usuário: ")
         nome <- getLine -- verificar se, já existe o usuário. Se existir, a pontuacao da partida será somada, do contrário, novo usuário precisa ser criado
-        novoJogo -- chamada a funcao do main.hs, iniciando novo jogo e suas respectivas partidas
-        else if opcaoEscolhidaNJ == 2 then
+        putStr("novoJogo") -- chamada a funcao do main.hs, iniciando novo jogo e suas respectivas partidas
+        else if opcaoEscolhidaNJ == "2" then
             menuPrincipal
         else do
-            putStr ("Opção Inválida")
-            go to menuNovoJogo --para que o programa não termine, e retorne ao mesmo menu.
+            putStr ("Opção Invalida \n")
+            menuNovoJogo --para que o programa não termine, e retorne ao mesmo menu.
 
 
 
@@ -56,18 +59,20 @@ menuNovoJogo : do
 -- Imprime apenas orientações da regra geral de negócio.
 menuComoJogar :: IO()
 menuComoJogar = do
+    putStr ("\n==============================================\n")
     putStr ("“Who am i?” é um jogo de turnos, você escolhe um personagem e o Bot irá receber outro personagem aleatoriamente.\n")
-    putStr ("A lista de personagens é mesma para ambos.\n" ++ "O objetivo do jogo é adivinhar o personagem do adversário, a cada rodada você pode:\n")
-    putStr ("Questionar uma característica ou pode tentar chutar o personagem escolhido pelo Bot.\n" ++ "Para vencer, é necessário adivinhar o personagem primeiro do que o Bot.\n\n\n")
-    putStr ("Informações importantes:\n" ++ "Você e o bot podem escolher o mesmo personagem.\nSe você escolher chutar e errar, você perde.\n\n")
+    putStr ("A lista de personagens é mesma para ambos.\nO objetivo do jogo é adivinhar o personagem do adversário, a cada rodada você pode:\n")
+    putStr ("Questionar uma característica ou pode tentar chutar o personagem escolhido pelo Bot.\nPara vencer, é necessário adivinhar o personagem primeiro do que o Bot.\n\n\n")
+    putStr ("Informações importantes:\nVocê e o bot podem escolher o mesmo personagem.\nSe você escolher chutar e errar, você perde.\n\n")
     putStr ("[1] Retornar ao Menu Principal\n")
 
+    putStr("Digite uma opcao: ")
     opcaoEscolhidaCJ <- getLine
 
-    if opcaoEscolhidaCJ == 1 then do 
+    if opcaoEscolhidaCJ == "1" then do 
         menuPrincipal
         else do 
-            putStr("Opção inválida")
+            putStr("Opção invalida \n")
             menuComoJogar --para que o programa não termine, e retorne ao mesmo menu.
 
 
@@ -76,15 +81,17 @@ menuComoJogar = do
 -- imprime o save file, ordenado, dos usuários cadastrados. -> USUARIO, Vitorias: X
 menuRanking :: IO()
 menuRanking = do
+    putStr ("\n==============================================\n")
     putStr ("Imprime aqui ranking dos top 10 jogadores cadastrados.\n\n") --Falta implementar
     
-    putStr ("[1] Retornar ao menu Principal")
+    putStr ("[1] Retornar ao menu Principal\n")
+    putStr("Digite uma opcao: ")
     opcaoEscolhidaMR <- getLine
 
-    if opcaoEscolhidaMR == 1 then do 
+    if opcaoEscolhidaMR == "1" then do 
         menuPrincipal
         else do
-            putStr ("Opção inválida")
+            putStr ("Opção invalida \n")
             menuRanking --para que o programa não termine, e retorne ao mesmo menu.
 
 
@@ -93,16 +100,18 @@ menuRanking = do
 -- encerra a execucao do programa na opção 1, retorna ao menu principal na posicao 0
 sairPrograma :: IO()
 sairPrograma = do
+    putStr ("\n==============================================\n")
     putStr ("Tem certeza que deseja sair?\n")
-    putStr ("[1] Sim")
-    putStr ("[0] Não")
+    putStr ("[1] Sim\n")
+    putStr ("[2] Não\n\n")
 
+    putStr("Digite uma opcao: ")
     opcaoEscolhidaSP <- getLine
 
-    if opcaoEscolhidaSP == 1 then do 
-        System.Exit(0)
-        else if opcaoEscolhidaSP == 0 then
+    if opcaoEscolhidaSP == "1" then do 
+        putStr("System.Exit(0)")
+        else if opcaoEscolhidaSP == "2" then
             menuPrincipal
         else do
-            putStr ("Opção inválida")
+            putStr ("Opção invalida \n")
             sairPrograma --para que o programa não termine, e retorne ao mesmo menu.
