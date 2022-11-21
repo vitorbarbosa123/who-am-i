485b629628c53fe93cb798b18d9c0da9861fceb5
vitor barbosa <vitorbarbosa789@gmail.com>
2022-11-21 11:40:29 -0300

    [ADD]  todo de separação de trabalho

--- src/Model/DecisionThree.hs
+++ src/Model/DecisionThree.hs
@@ -1,3 +1,38 @@
+-- Todo: criar uma função que gera um número aleatório entre 0 e o tamanho de uma
+-- lista genérica
+
+-- Todo: criar uma função que usa o número aleatório anterior como indice para buscar
+-- um elemento em algum array
+
+
+-- Todo: criar funções que usa a anterior(deve-se passar o array específico)
+-- e retorna o elemento em um indice:
+
+-- Todo: função que busca o sexo
+
+-- Todo: função que busca a cor dos olhos
+
+-- Todo: função que busca a cor da pele
+
+-- Todo: função que busca a cor do cabelo
+
+-- Todo: função que busca o acessório
+
+
+-- Todo: uma função que salva a decisão feita em um novo array
+
+-- Todo: uma função que verifica se a persona "montada" é a mesma que a escolhida
+
+-- Todo: uma função que utiliza a da linha 22 e repassa para as funções da linha 11
+-- em diante que: caso o elemento retornado seja igual ao já selecionado anteriormente
+-- validar se o valor booleano do mesmo é false(ou seja, pode ser selecionado de novo)
+-- se for, a função vai "rodar" novamente
+
+
+
+
+
+
 -- Imprime reticências na tela para humanizar a tomada de decisão da IA. Perguntar ao professor ou monitor se a definição do método 
 -- pode utilizar um valor concreto ao invés de um tipo
 
