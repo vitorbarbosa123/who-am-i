f6da3aba8c3e655329bb17760e86f2e5577ae824
Cleciana <cleciana.santana@ccc.ufcg.edu.br>
2022-11-26 10:52:31 -0300

    remove codigo nao usado

--- app/Model/DecisionThree.hs
+++ app/Model/DecisionThree.hs
@@ -109,17 +109,3 @@ funcoesRestantes [categoria:caudaC] [acerto:caudaA] [funcao:caudaF] =
 
 adicionaTupla::(T,Bool)->[T]->[T]
 adicionaTupla tupla lista = [tupla] ++ lista
-
--- Imprime reticências na tela para humanizar a tomada de decisão da IA. Perguntar ao professor ou monitor se a definição do método
--- pode utilizar um valor concreto ao invés de um tipo
-
--- esperaIA::Integer -> () -- Sempre iniciar com 0
--- esperaIA inicio,totalReticencias =
---     if inicio < 3
---         then do threadDelay 300000 -- Aprox. 0.3 segundos
---                 threadDelay 300000 putStr(". ")
---                 threadDelay 300000 putStr(". ")
---                 threadDelay 300000 putStrLn(".")
---                 threadDelay 300000
---                 esperaIA inicio+1 totalReticencias
---         else putStrLn("!")
