b6951a8e733798b8028599163e22bae209f6fc77
Cleciana <cleciana.santana@ccc.ufcg.edu.br>
2022-11-27 22:27:48 -0300

    corrigindo maioria dos erros de tipo

--- app/Model/DecisionTree.hs
+++ app/Model/DecisionTree.hs
@@ -1,7 +1,6 @@
 module DecisionTree(
     getTraits,
     getFunction,
-    getLength,
     renderRandom,
     getElemByIndex,
     getSex,
@@ -10,7 +9,8 @@ module DecisionTree(
     getEyes,
     getProps,
     saveChoices,
-    verifyPersona
+    verifyPersona,
+    remainingFunctions
 ) where
 
 import System.Random
@@ -31,45 +31,39 @@ getFunction::[String]
 getFunction = ["getSex", "getHair", "getSkin", "getEyes", "getProps"]
 
 -- Gera um número aleatório entre 0 e o tamanho de uma lista genérica
-renderRandom:: [T] -> Int
+renderRandom::[String] -> Int
 renderRandom array =
    randomRIO (1, length array)
 
 -- Usa o número aleatório como indice para buscar um elemento em algum array
-getElemByIndex:: [T] -> String
-getElemByIndex list result =
-    result list!!index
+getElemByIndex::[String] -> String
+getElemByIndex list =
+    list!!index
     where index = renderRandom list
 
-getSex:: [String]->String
-getSex sex result =
-    result getElemByIndex sex
+getSex::[String]->String
+getSex sex = getElemByIndex sex
 
-getEyes:: [String]->String
-getEyes eyes result =
-    result getElemByIndex eyes
+getEyes::[String]->String
+getEyes eyes = getElemByIndex eyes
 
-getSkin:: [String]->String
-getSkin skin result =
-    result getElemByIndex skin
+getSkin::[String]->String
+getSkin skin = getElemByIndex skin
 
-getHair:: [String]->String
-getHair hair result =
-    result getElemByIndex hair
+getHair::[String]->String
+getHair hair = getElemByIndex hair
 
-getProps:: [String]->String
-getProps props result =
-    result getElemByIndex props
+getProps::[String]->String
+getProps props = getElemByIndex props
 
 -- Salva a decisão feita em um novo array
-saveChoices:: [String] -> [String] -> [String] -> [String] -> [String] -> [String]
+saveChoices::[String] -> [String] -> [String] -> [String] -> [String] -> [String]
 saveChoices sex hair skin eyes props =
-    [getSex sex
-    getHair hair
-    getSkin skin
-    getEyes eyes
+    [getSex sex,
+    getHair hair,
+    getSkin skin,
+    getEyes eyes,
     getProps props]
-    
 
 -- Verifica se a persona "montada" é a mesma que a escolhida
 verifyPersona::[String]->[String]->Bool
@@ -79,27 +73,27 @@ verifyPersona persona personaCorreta =
 -- Retorna uma lista com as caracteristicas da persona "montada" que estao na
 -- persona correta ou a string "erro" na ordem: (sex, hair, skin, eyes, props)
 verifyTraits::[String]->[String]->Int->[String]
-verifyTraits personaMontada personaCorreta i =
+verifyTraits personaMontada personaCorreta i
     | (null caracteristica) = []  -- fim da lista / condicao de parada
 
     | otherwise =
-        | (caracteristica  == personaCorreta!!i) =
+        if (caracteristica == personaCorreta!!i) then
             [caracteristica] ++ (verifyTraits personaMontada personaCorreta i+1)
 
-        | otherwise =
+        else
             ["erro"] ++ (verifyTraits personaMontada personaCorreta i+1)
     where caracteristica = personaMontada!!i
 
 -- Retorna uma lista com nomes das funcoes que ainda podem rodar
-funcoesRestantes::[[String]]->[String]->[String]->[(T,Boolean)]
-funcoesRestantes [categoria:caudaC] [acerto:caudaA] [funcao:caudaF] =
-    | (categoria == []) = []     -- fim da lista / condicao de parada
+remainingFunctions::[[String]]->[String]->[String]->[(String,Bool)]
+remainingFunctions categorias escolhasJogador funcoes
+    | (null caracteristica) = []     -- fim da lista / condicao de parada
 
-    | acerto `elem` categoria =  -- se acertou a categoria:
-        adicionaTupla (funcao, True) (funcoesRestantes caudaC caudaA caudaF)  -- adiciona (nomeDaFuncao, True) a resposta
+    | caracteristica `elem` categoria =           -- se acertou a categoria:
+        [((head funcoes), True)] ++ (remainingFunctions (tail categorias) (tail escolhasJogador) (tail funcoes)) -- adiciona (nomeDaFuncao, True) a resposta
 
     | otherwise =
-        adicionaTupla (funcao, False) (funcoesRestantes caudaC caudaA caudaF) -- adiciona (nomeDaFuncao, False) a resposta
-
-adicionaTupla::(T,Bool)->[T]->[T]
-adicionaTupla tupla lista = [tupla] ++ lista
+        [((head funcoes), False)] ++ (remainingFunctions (tail categorias) (tail escolhasJogador) (tail funcoes)) -- adiciona (nomeDaFuncao, False) a resposta
+    where
+        categoria = head categorias
+        caracteristica = head escolhasJogador
