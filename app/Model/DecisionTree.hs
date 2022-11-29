797171e5360348b794b462f3fa8b6e29daf8ebb6
80813598/Vitor Barbosa <jose.vitor.b.maciel@accenture.com>
2022-11-29 19:19:24 -0300

    [FIX] salvando decisoes em array + limites de tamanho para decisao

--- app/Model/DecisionTree.hs
+++ app/Model/DecisionTree.hs
@@ -9,8 +9,6 @@ module Model.DecisionTree(
     getEyes,
     getProps,
     saveChoices,
-    verifyPersona,
-    remainingFunctions
 ) where
 
 import System.Random
@@ -18,9 +16,9 @@ import Data.List
 
 sex = ["Feminino","Masculino"]
 hair = ["Castanho","Louro","Preto","Ruivo"]
-skin = ["Branca","Indígena","Negra","Parda"]
+skin = ["Branca","Indigena","Negra","Parda"]
 eyes = ["Azuis","Castanhos","Pretos","Verdes"]
-props = ["Brinco","Chapéu","Óculos","Piercing"]
+props = ["Brinco","Chapeu","Oculos","Piercing"]
 
 -- Guarda as caracteristicas possíveis de cada categoria
 getTraits::[[String]]
@@ -33,7 +31,7 @@ getFunction = ["getSex", "getHair", "getSkin", "getEyes", "getProps"]
 -- Gera um número aleatório entre 0 e o tamanho de uma lista genérica
 renderRandom::[String] -> IO Int 
 renderRandom array =
-   randomRIO (1, length array)
+   randomRIO (0, length array - 1)
 
 -- Usa o número aleatório como indice para buscar um elemento em algum array
 getElemByIndex::[String] -> IO String
@@ -44,19 +42,20 @@ getElemByIndex list = do
 getSex::[String]-> IO String
 getSex sex = do
     result <- getElemByIndex sex
-    return result 
+    return result
 
-getEyes::[String]->IO String
+getEyes::[String]-> IO String
 getEyes eyes = do
     result <- getElemByIndex eyes
     return result
+   
 
 getSkin::[String]-> IO String
 getSkin skin = do 
     result <- getElemByIndex skin
     return result
 
-getHair::[String]->IO String
+getHair::[String]-> IO String
 getHair hair = do 
     result <- getElemByIndex hair
     return result
@@ -68,51 +67,53 @@ getProps props = do
 
 -- Salva a decisão feita em um novo array
 
+
 -- como fazer referência aos lets declarados no inicio?
-saveChoices::[String] -> [String] -> [String] -> [String] -> [String] ->  IO [String]
-saveChoices sex hair skin eyes props = do
+saveChoices::[String] -> [String] -> [String] -> [String] -> [String] -> IO()
+saveChoices sex hair skin eyes props = do             
     sexChoice <- getSex sex
     hairChoice <- getHair hair
     skinChoice <- getSkin skin
     eyesChoice <- getEyes eyes
     propsChoice <- getProps props
-    return [sexChoice, hairChoice, skinChoice, eyesChoice, propsChoice]
+    putStrLn $ show [sexChoice, hairChoice, skinChoice, eyesChoice, propsChoice]
 
--- Verifica se a persona "montada" é a mesma que a escolhida
-verifyPersona::[String]->[String]->Bool
-verifyPersona persona personaCorreta =
-    personaCorreta == (verifyTraits persona personaCorreta 0)
-    -- passar ele pra main, para dizer que a IA ganhou
 
--- Retorna uma lista com as caracteristicas da persona "montada" que estao na
--- persona correta ou a string "erro" na ordem: (sex, hair, skin, eyes, props)
-verifyTraits::[String]->[String]->Int->[String]
-verifyTraits personaMontada personaCorreta i
-    | (null caracteristica) = []  -- fim da lista / condicao de parada
+-- -- -- Verifica se a persona "montada" é a mesma que a escolhida
+--  verifyPersona::[String]->[String]->Bool
+--  verifyPersona persona personaCorreta =
+--      personaCorreta == (verifyTraits persona personaCorreta 0)
+--      -- passar ele pra main, para dizer que a IA ganhou
 
-    | otherwise =
-        if (caracteristica == personaCorreta!!i) then
-            [caracteristica] ++ (verifyTraits personaMontada personaCorreta i+1)
+-- -- -- Retorna uma lista com as caracteristicas da persona "montada" que estao na
+-- -- -- persona correta ou a string "erro" na ordem: (sex, hair, skin, eyes, props)
+--  verifyTraits::[String]->[String]->Int->[String]
+--  verifyTraits personaMontada personaCorreta i
+--      | (null caracteristica) = []   -- fim da lista / condicao de parada
 
-        else
-            ["erro"] ++ (verifyTraits personaMontada personaCorreta i+1)
-    where caracteristica = personaMontada!!i
+--      | otherwise =
+--          if (caracteristica == personaCorreta!!i) then
+--              [caracteristica] ++ (verifyTraits personaMontada personaCorreta $ i+1)
 
--- essa função deve retornar um arrays de função.
--- Retorna uma lista com nomes das funcoes que ainda podem rodar
-remainingFunctions::[[String]]->[String]->[String]->[String]
-remainingFunctions categorias escolhasJogador funcoes
-    | (null caracteristica) = []     -- fim da lista / condicao de parada
+--          else
+--              ["erro"] ++ (verifyTraits personaMontada personaCorreta $ i+1)
+--      where caracteristica = personaMontada!!i
 
-    | caracteristica `elem` categoria =           -- se acertou a categoria:
-        [head funcoes] ++ (remainingFunctions (tail categorias) (tail escolhasJogador) (tail funcoes)) -- adiciona (nomeDaFuncao, True) a resposta
+-- -- -- essa função deve retornar um arrays de função.
+-- -- -- Retorna uma lista com nomes das funcoes que ainda podem rodar
+--  remainingFunctions::[[String]]->[String]->[String]->[String]
+--  remainingFunctions categorias escolhasJogador funcoes
+--      | (null caracteristica) = []     -- fim da lista / condicao de parada
 
-    | otherwise =
-        (remainingFunctions (tail categorias) (tail escolhasJogador) (tail funcoes)) -- adiciona (nomeDaFuncao, False) a resposta
-    where
-        categoria = head categorias -- sex
-        caracteristica = head escolhasJogador -- "M"
+--      | caracteristica `elem` categoria =           -- se acertou a categoria:
+--          [head funcoes] ++ (remainingFunctions (tail categorias) (tail escolhasJogador) (tail funcoes))
 
-        -- [getSex, getSkin]
+--      | otherwise =
+--          (remainingFunctions (tail categorias) (tail escolhasJogador) (tail funcoes))
+--      where
+--          categoria = head categorias  sex
+--          caracteristica = head escolhasJogador -- "M"
+
+-- --         -- [getSex, getSkin]
         
 
