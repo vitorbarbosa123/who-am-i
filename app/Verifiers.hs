0052b35ed6f69a41d06824cb9239a8db093a542f
Cleciana <cleciana.santana@ccc.ufcg.edu.br>
2022-11-29 19:22:52 -0300

    correções em verifiers

--- app/Verifiers.hs
+++ app/Verifiers.hs
@@ -1,38 +1,15 @@
-module Verifiers where
+module Verifiers(
+    removePersonagens,
+    verificacaoPersonagem
+) where
 
 
--- Itera sobre uma lista de personagens, eliminando personagens
-removePersonagens:: [String] -> Int -> String -> () --Argumentos: lista de personagens a ser iterada, iterador/condição de parada, característica
-removePersonagens lista iterador caracteristica = do 
-    if iterador < (length lista)
-        then if caracteristica `elem` (lista !! iterador)
-            then removePersonagem lista iterador
-            else removePersonagens lista (iterador+1)
-        else putStr ("Erro aqui!")
-    
+-- Itera sobre uma lista de personagens, eliminando personagens que não tenham uma determinada caracteristica
+removePersonagens:: [[String]] -> String -> [[String]] --Argumentos: lista de personagens a ser iterada e característica
+removePersonagens lista caracteristica =
+    [personagem | personagem <- lista, caracteristica `elem` personagem]
+
 
 -- Verifica se o personagem do adversário possui determinada característica física
-verificacaoPersonagem::[String] -> String -> Int -> Bool
-verificacaoPersonagem listaPersonagens caracteristica indicePersonagem = do
-    if caracteristica `elem` (listaPersonagens !! indicePersonagem)
-        then True
-        else False
-
-
--- Remove um personagem da lista de uma partida
-removePersonagem::[String] -> Int -> ()
-removePersonagem listaPersonagens indice = do
-    removePorIndice indice listaPersonagens
-
-
--- Remove um elemento de uma lista de acordo com o índice fornecido
-removePorIndice :: Int -> [String] -> [String]
-removePorIndice = \n -> \list ->
-    case n of 
-        0 -> tail list
-        otherwise -> head list: removePorIndice (n-1) (tail list)
-
-
---alteraListaCaracteristicas:: Bool -> String -> Integer -> ()
---alteraListaCaracteristicas operador caracteristica indice = do 
---    Nothing
+verificacaoPersonagem::String -> Int -> [[String]]-> Bool
+verificacaoPersonagem caracteristica indicePersonagem listaPersonagens = caracteristica `elem` listaPersonagens !! indicePersonagem
