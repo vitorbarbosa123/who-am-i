fcea9414d491a0837d1f9a1503a3e85f786ec3b3
Daviiap <davii.sapiens@gmail.com>
2022-11-29 22:12:07 -0300

    feat: adicionando palpite do usuario

--- app/Main.hs
+++ app/Main.hs
@@ -34,27 +34,61 @@ jogo user = do
     let possibilidadesUser = map parsePersonaToStringArray personasField
     let possibilidadesIA = map parsePersonaToStringArray personasField
 
+    print personaIA
+
     partida user personaUsuario personaIA possibilidadesUser possibilidadesIA "user"
 
     main
 
+pegarPalpiteUser :: IO String
+pegarPalpiteUser = do
+    print "Qual caracteristica voce deseja escolher?"
+    print "sexo | cor_cabelo | etnia | cor_olhos | acessorio"
+    caracteristica <- getLine
+
+    if(caracteristica == "sexo") then do
+        print "Qual o sexo?"
+        getLine
+    else if(caracteristica == "cor_cabelo") then do
+        print "Qual a cor do cabelo?"
+        getLine
+    else if(caracteristica == "etnia") then do
+        print "Qual a etnia?"
+        getLine
+    else if(caracteristica == "cor_olhos") then do
+        print "Qual a cor dos olhos?"
+        getLine
+    else if(caracteristica == "acessorio") then do
+        print "Qual o acessorio?"
+        getLine
+    else 
+        pegarPalpiteUser
+        
 partida :: [String] -> [String] -> [String] -> [[String]] -> [[String]] -> String -> IO()
 partida user personaUsuario personaIA possibilidadesUser possibilidadesIA jogadorDaVez = do
-    if((length possibilidadesUser) == 1) then do
+    if((length possibilidadesUser) <= 1) then do
         somaPontosUsuario (user!!0) 1
         print "parabens! voce ganhou!"
-    else if((length possibilidadesIA) == 1) then
+    else if((length possibilidadesIA) <= 1) then
         print "sinto muito... voce perdeu."
     else
-        if(jogadorDaVez == "user") then
+        if(jogadorDaVez == "user") then do
             printTable possibilidadesUser
             palpite <- pegarPalpiteUser
-            newPossibilidades <- verificarPalpite palpite personaIA possibilidadesUser
+            let newPossibilidades = verificarPalpite palpite personaIA possibilidadesUser
             partida user personaUsuario personaIA newPossibilidades possibilidadesIA "ia"
-        else
-            palpite <- pegarPalpiteIA
-            newPossibilidades <- verificarPalpite palpite personaUsuario possibilidadesIA
+        else do
+            -- palpite <- pegarPalpiteUser
+            -- let newPossibilidades = verificarPalpite palpite personaUsuario possibilidadesIA
+            let newPossibilidades = tail possibilidadesIA
             partida user personaUsuario personaIA possibilidadesUser newPossibilidades "user"
+
+verificarPalpite :: String -> [String] -> [[String]] -> [[String]]
+verificarPalpite palpite persona possibilidades = do
+    if(elem palpite persona) then
+        [personagem | personagem <- possibilidades, elem palpite personagem]
+    else
+        [personagem | personagem <- possibilidades, not (elem palpite personagem)]
 
 showPlacar :: IO ()
 showPlacar = do
