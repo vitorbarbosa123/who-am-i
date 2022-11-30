309f0a91c07cd1311c3d2f1b2c3e45d1ff5bcfb5
Daviiap <davii.sapiens@gmail.com>
2022-11-30 09:24:13 -0300

    refactor: aplicando sujestoes da ide no codigo

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,5 @@
 module Main where
 
-import System.Random
 import Repo.Usuarios
 import Repo.Personas
 
@@ -20,21 +19,20 @@ novoJogo = do
     print "Digite o nome do jogador 2"
     username2 <- getLine
 
-    if(username1 == username2) then do
+    if username1 == username2 then do
         putStr "ERRO: Nomes nao podem ser iguais. Pressione Enter para continuar...\n"
         getLine
         novoJogo
     else do
         userField <- criaUsuarioSeNaoExistir username1
-        let user1 = parseUserToStringArray (userField!!0)
+        let user1 = parseUserToStringArray (head userField)
 
         userField <- criaUsuarioSeNaoExistir username2
-        let user2 = parseUserToStringArray (userField!!0)
+        let user2 = parseUserToStringArray (head userField)
 
         jogo user1 user2
 
         main
-    
 
 escolhePersonagem :: [String] -> [[String]] -> IO String
 escolhePersonagem user personas = do
@@ -70,23 +68,23 @@ jogo user1 user2 = do
 
 pegarPalpiteUser :: [String] -> IO String
 pegarPalpiteUser user = do
-    putStr ("Qual caracteristica voce deseja escolher " ++ user!!0 ++ "?\n")
+    putStr ("Qual caracteristica voce deseja escolher " ++ head user ++ "?\n")
     putStr "[0] sexo | [1] cor do cabelo | [2] etnia | [3] cor dos olhos | [4] acessorio\n"
     caracteristica <- getLine
 
-    if(caracteristica == "0") then do
+    if caracteristica == "0" then do
         putStr "Qual o sexo?\n"
         getLine
-    else if(caracteristica == "1") then do
+    else if caracteristica == "1" then do
         putStr "Qual a cor do cabelo?\n"
         getLine
-    else if(caracteristica == "2") then do
+    else if caracteristica == "2" then do
         putStr "Qual a etnia?\n"
         getLine
-    else if(caracteristica == "3") then do
+    else if caracteristica == "3" then do
         putStr "Qual a cor dos olhos?\n"
         getLine
-    else if(caracteristica == "4") then do
+    else if caracteristica == "4" then do
         putStr "Qual o acessorio?\n"
         getLine
     else do
@@ -95,24 +93,24 @@ pegarPalpiteUser user = do
 
 vitoria :: [String] -> [String] -> [String] -> IO()
 vitoria userGanhador oponente personaOponente = do
-    somaPontosUsuario (userGanhador!!0) 1
+    somaPontosUsuario (head userGanhador) 1
 
     cls
     putStr "|=======================================================================================|\n"
     putStr "|======================================| VICTORY! |=====================================|\n"
     putStr "|=======================================================================================|\n\n"
-    putStr ("PARABENS " ++ userGanhador!!0 ++ "! VOCE GANHOU!\n")
-    putStr ("O personagem de " ++ oponente!!0 ++ " era: " ++ (prepareTableLine personaOponente) ++ "\n")
+    putStr ("PARABENS " ++ head userGanhador ++ "! VOCE GANHOU!\n")
+    putStr ("O personagem de " ++ head oponente ++ " era: " ++ prepareTableLine personaOponente ++ "\n")
     putStr "Pressione Enter para voltar ao menu principal...\n"
     getLine
     putStr ""
 
 userGanhou :: [[String]] -> Bool
-userGanhou listaPossibilidades = (length listaPossibilidades) <= 1
+userGanhou listaPossibilidades = length listaPossibilidades <= 1
 
 partida :: [String] -> [String] -> [String] -> [String] -> [[String]] -> [[String]] -> IO()
 partida jogadorDaVez oponente personaJogadorDaVez personaOponente possibilidadesJogadorDaVez possibilidadesOponente = do
-    if(userGanhou possibilidadesOponente) then do
+    if userGanhou possibilidadesOponente then do
         vitoria oponente jogadorDaVez personaJogadorDaVez
     else do
         cls
@@ -122,7 +120,7 @@ partida jogadorDaVez oponente personaJogadorDaVez personaOponente possibilidades
 
         palpite <- pegarPalpiteUser jogadorDaVez
         let newPossibilidades = verificarPalpite palpite personaOponente possibilidadesJogadorDaVez
-        if((length newPossibilidades) == (length possibilidadesJogadorDaVez)) then do
+        if length newPossibilidades == length possibilidadesJogadorDaVez then do
             putStr "OPCAO INVALIDA! Pressione Enter para tentar novamente...\n"
             getLine
             partida jogadorDaVez oponente personaJogadorDaVez personaOponente possibilidadesJogadorDaVez possibilidadesOponente
@@ -131,10 +129,10 @@ partida jogadorDaVez oponente personaJogadorDaVez personaOponente possibilidades
 
 verificarPalpite :: String -> [String] -> [[String]] -> [[String]]
 verificarPalpite palpite persona possibilidades = do
-    if(elem palpite persona) then
-        [personagem | personagem <- possibilidades, elem palpite personagem]
+    if palpite `elem` persona then
+        [personagem | personagem <- possibilidades, palpite `elem` personagem]
     else
-        [personagem | personagem <- possibilidades, not (elem palpite personagem)]
+        [personagem | personagem <- possibilidades, palpite `notElem` personagem]
 
 showPlacar :: IO ()
 showPlacar = do
@@ -153,11 +151,12 @@ showPlacar = do
     main
 
 printTable :: [[String]] -> IO()
-printTable table = mapM_ putStr (map prepareTableLine table)
+printTable = mapM_ (putStr . prepareTableLine)
 
 prepareTableLine :: [String] -> String
-prepareTableLine (head:[]) = head ++ "\n"
-prepareTableLine (head:users) = ("   " ++ head ++ "  |  " ++ (prepareTableLine users))
+prepareTableLine [] = ""
+prepareTableLine [head] = head ++ "\n"
+prepareTableLine (head:users) = "   " ++ head ++ "  |  " ++ prepareTableLine users
 
 pegaEscolhaInicial :: IO String
 pegaEscolhaInicial = do
@@ -173,9 +172,9 @@ pegaEscolhaInicial = do
 main :: IO ()
 main = do
     escolha <- pegaEscolhaInicial
-    if(escolha == "1") then
+    if escolha == "1" then
         novoJogo
-    else if(escolha == "2") then
+    else if escolha == "2" then
         showPlacar
     else
         main
