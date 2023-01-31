18dd321b92b4375edfd30fdc2b5dad963097dc4d
vitor barbosa <vitorbarbosa789@gmail.com>
2023-01-31 02:47:17 -0300

    [FIX] path

--- Prolog/main.pl
+++
@@ -1,9 +0,0 @@
-:- use_module('./src/Interface.pl').
-:- use_module('./src/Model.pl').
-:- use_module('./src/Utils.pl').
-:- use_module('./src/Repository.pl').
-
-main :-
-    connect_repo,
-    findUsuarios(X),
-    write_output(X).
