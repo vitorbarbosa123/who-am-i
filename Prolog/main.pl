00fa7778a66a747d81b9bf520812ccb5b02cd492
vitor barbosa <vitorbarbosa789@gmail.com>
2023-02-01 21:09:13 -0300

    [ADD] main

--- Prolog/main.pl
+++ Prolog/main.pl
@@ -1,9 +1,10 @@
-:- use_module('./src/Interfaces/interface.pl').
+:- use_module('./src/Interfaces/menus.pl').
 :- use_module('./src/Model/model.pl').
 :- use_module('./src/Utils/utils.pl').
 :- use_module('./src/Data/repository.pl').
 
 main :-
     connect_repo,
+    mainMenu,
     findUsuarios(X),
     write_output(X).
