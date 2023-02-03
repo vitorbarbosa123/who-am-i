8f8f15055bf7f9d4b31d0e4ca005e2e8a7316506
vitor barbosa <vitorbarbosa789@gmail.com>
2023-02-03 04:01:21 -0300

    [FIX] bot choice

--- Prolog/src/Model/model.pl
+++ Prolog/src/Model/model.pl
@@ -2,7 +2,7 @@
   renderRandom/2,
   getElemByIndex/2,
   tail/2,
-  pegarPalpiteIA/2
+  pegarPalpiteIA/1
 ]).
 
 :- use_module('./../Data/repository.pl').
@@ -16,12 +16,10 @@ O método findPersonas(X) já está retornando as personas
 */
 % Gera um número aleatório dentro do range de Lista
 renderRandom(Lista, R) :- 
-  connect_repo,
   length(Lista, L), Limit is L-1, random(0, Limit, X), R is X.
 
 % Retorna um elemento aleatório de Lista
 getElemByIndex(Lista, Elem) :- 
-  connect_repo,
   renderRandom(Lista, I), nth0(I, Lista, E, _), Elem = E.
 
 % Retorna uma lista sem o primeiro elemento
@@ -29,11 +27,10 @@ tail([_|T], T).
 
 /* 'Funcao' que determina aleatoriamente o palpite que sera feito pelo bot, de acordo com as possibilidades restantes. Utiliza as funcoes getElemByIndex e tail.
   @Param: lista dos personagens disponiveis.*/
-pegarPalpiteIA(ListaPersonagens, Palpite) :-
+pegarPalpiteIA(Palpite) :-
   connect_repo,
   findPersonas(ListaPersonagens),
   getElemByIndex(ListaPersonagens, P),
   tail(P, T1),
   tail(T1, T2),
-  Palpite = getElemByIndex(T2),
-  write(Palpite).
+  getElemByIndex(T2, Palpite).
